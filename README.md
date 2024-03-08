5d778df026939c8eae6c3386fe6e89d6f0be9e69
Olin <olin.costa@sou.inteli.edu.br>
2024-03-08 16:16:36 -0300

    adiciona funcionalidades

---
+++ README.md
@@ -0,0 +1,14 @@
+# Prova 1 Ólin
+
+## Descrição
+
+Este repositório contém o código relacionado à uma aplicacão web capaz de salvar posicões de um robô.
+
+## Uso
+
+1. **Instalação de Dependências**: Certifique-se de ter todas as dependências instaladas. Para tal, execute o comando `pip install -r requirements. txt`
+3. **Execução do Programa**: Execute o programa principal (Execute o mesmo através de python3/python src/app.py). 
+
+## Autor
+
+Este código foi desenvolvido por Ólin Medeiros Costa.
---
+++ src/app.py
@@ -0,0 +1,53 @@
+# app.py
+from flask import Flask, render_template, request, redirect, url_for
+from tinydb import TinyDB
+
+app = Flask(__name__)
+
+db = TinyDB("caminhos.json")
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/novo")
+def novo2():
+    return render_template("novo.html")
+    
+
+
+@app.route("/novo" , methods=["GET", "POST"])
+def novo():
+    if request.method == "POST":
+        id = request.form.get("id")
+        nome = request.form.get("nome")
+        coordenadas = request.form.get("coordenadas")
+        db.insert({"id": id,"nome": nome, "coordenadas": coordenadas})
+
+    return render_template("index.html")
+
+
+@app.route("/listar_caminhos")
+def listar():
+    posts = db.all()
+    return render_template("caminhos.html",posts=posts)
+
+@app.route("/buscar_caminho")
+def buscar():
+    return render_template("buscar-caminho.html")
+
+@app.route("/pegar_caminho", methods=["GET", "POST"])
+def listar_by_id():
+    id = request.form.get("id")
+    caminho = db.get(id == 'id')
+    return render_template("caminho.html",caminho = caminho)
+
+
+    
+
+
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
